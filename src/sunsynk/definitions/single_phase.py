103e0cc500dc44ac5f553db3761cc5de68ac7b98
ox <mr.artemio@gmail.com>
2026-03-30 21:54:52 +0300

    feat: add single-phase SmartLoad (AUX) configuration entities

--- src/sunsynk/definitions/single_phase.py
+++ src/sunsynk/definitions/single_phase.py
@@ -179,6 +179,9 @@ SENSORS += (
     SelectRWSensor(
         235, "Generator input", "", options={0: "Disable", 1: "Output", 2: "Input"}
     ),
+    NumberRWSensor(135, "Generator Off SOC", "%"),
+    NumberRWSensor(137, "Generator On SOC", "%"),
+    SwitchRWSensor(195, "SmartLoad Enable Status", on=1),
     Sensor(312, "Battery charging voltage", VOLT, 0.01),
     Sensor(603, "Bat1 SOC", "%"),
     Sensor(611, "Bat1 Cycle"),
